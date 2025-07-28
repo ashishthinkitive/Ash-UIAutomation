3243b149c4d591bb773b276ba0a5e9cbeb1e5b58
Ashish Kulkarni <ashish.kulkarni@thinkitive.com>
2025-07-28 18:52:55 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,6 +47,7 @@ export default defineConfig({
       use: { 
         ...devices['Desktop Chrome'],
         viewport: { width: 1280, height: 720 },
+        headless: true,
       },
     },
   ],
